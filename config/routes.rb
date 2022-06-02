2d18ac538d0bbee6baf52a9f2d8f0d9c0059a3d4
Everton Santos <everton.santos@redealumni.com>
2022-06-02 20:02:12 -0300

    Routes an readme adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :jobs
-  resources :profiles
   devise_for :users, :controllers => { registration: 'registration' }
   root to: "home#index"
-
+  
+  resources :jobs
+  resources :profiles
 
 end
